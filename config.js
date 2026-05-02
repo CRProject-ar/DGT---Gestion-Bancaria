b2eb2993d0f41db77516724cca0e3044d4ccd51a
CRProject-ar <contacto@cristianrios.com.ar>
2026-05-02 19:49:05 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 // Reemplazá estos valores con los de tu proyecto en Supabase
 // Dashboard → Settings → API
 // ============================================================
-const SUPABASE_URL = 'https://tcrxlorirdiqxdbddvwg.supabase.co/rest/v1/';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjcnhsb3JpcmRpcXhkYmRkdndnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3MzczMDgsImV4cCI6MjA5MzMxMzMwOH0.Ivv8TZ6eiq-nxYk80XspkkllHY-Jd5kht_gQ1sF8uaE';
+const SUPABASE_URL = https://tcrxlorirdiqxdbddvwg.supabase.co/rest/v1/;
+const SUPABASE_ANON_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjcnhsb3JpcmRpcXhkYmRkdndnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3MzczMDgsImV4cCI6MjA5MzMxMzMwOH0.Ivv8TZ6eiq-nxYk80XspkkllHY-Jd5kht_gQ1sF8uaE;
